59cc448e9f13b0286712ba3c685980a29dcc8c61
Евгений Щепров <evgenyscheprov@gmail.com>
2019-11-23 12:22:10 +0300

    1

---
+++ pavl_5/pavl_5.cpp
@@ -0,0 +1,71 @@
+﻿
+#include <iostream>
+#include<cstring>
+using namespace std;
+
+struct price { //только английский
+	
+	char name[20];
+	char shop[20];
+	int cost;
+};
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	const int SIZE = 4;
+	int i,k,itemp=0;
+	char *min,tovar[20];
+	price arr[SIZE];
+	price temp;
+	bool find=false;
+	for (i = 0; i < SIZE; i++)
+	{
+		cout << "введите название товара: ";
+		cin >> arr[i].name;
+		cout << "введите название магазина: ";
+		cin >> arr[i].shop;
+		cout << "введите стоимость товара: ";
+		cin >> arr[i].cost;
+	}
+
+	for (i = 0; i<SIZE - 1; i++)		//сортировка
+	{
+		min = arr[i].name;
+		for (k = i + 1; k < SIZE; k++)
+		{
+			if (strcmp(arr[k].name, min) == -1) 
+			{
+				min = arr[k].name; 
+				itemp = k; 
+				
+			}
+		}
+		temp = arr[i];
+		arr[i] = arr[itemp];
+		arr[itemp] = temp;
+	}
+	
+	cout << "Сортировка: " << endl;
+for (i = 0; i < SIZE; i++)
+{
+	cout << arr[i].name << endl;
+	cout << arr[i].shop << endl;
+	cout << arr[i].cost << endl;
+}
+	cout << "введите название товара: "; //поиск товара
+
+	cin >> tovar;
+	for (i = 0; i < SIZE; i++)
+	{
+		if (arr[i].name==tovar)
+			{
+			find = true;
+			cout << "информация о товаре:" << endl;
+			cout << arr[i].name << endl;
+			cout << arr[i].shop << endl;
+			cout << arr[i].cost << endl;
+			}
+
+	}
+	if (find == false) { cout << "товар не найден"; }
+}
